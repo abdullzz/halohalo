37c98d673facd0bbdad4ef672a3adb3c78aa155e
abdullah <abdullah.said@bukalapak.com>
2021-07-23 22:16:24 +0700

    update client/initialize auth UI + Bootstrap + Navbar

--- client/config/routes.rb
+++ client/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'auth/login'
+  get 'auth/register'
+  get 'auth/privacy'
+  get 'auth/terms'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
