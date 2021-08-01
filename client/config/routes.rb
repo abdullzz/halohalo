366e81e7ee883bc3e29baaa3bd0b91258298117d
abdullah <abdullah.said@bukalapak.com>
2021-08-02 04:33:07 +0700

    Test commit

--- client/config/routes.rb
+++ client/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   get 'logout', to: 'session#destroy'
   get '/privacy', to: 'session#privacy'
   get '/terms', to: 'session#terms'
-  get '/profile', to: 'users#profile'
+  get '/user/profile', to: 'users#profile'
   get '/user/dashboard', to: 'users#dashboard'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
